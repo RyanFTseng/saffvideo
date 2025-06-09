565e23e16750c5455c8f20289e56049465d27657
Ryan Tseng <66948916+RyanFTseng@users.noreply.github.com>
2025-06-09 11:23:08 -0700

    Update saff.py

--- saff.py
+++ saff.py
@@ -57,3 +57,60 @@ decoder_srcpad.link(streammux_sinkpad)
 streammux.link(pgie)
 pgie.link(fakesink)
 print('Linked elements in pipeline')
+
+# Declare list to hold count data
+obj_counts=[]
+
+# Define the Probe Function
+def pgie_src_pad_buffer_probe(pad, info):
+    gst_buffer=info.get_buffer()
+
+    # Retrieve batch metadata from the gst_buffer
+    # Note that pyds.gst_buffer_get_nvds_batch_meta() expects the
+    # C address of gst_buffer as input, which is obtained with hash(gst_buffer)
+    batch_meta=pyds.gst_buffer_get_nvds_batch_meta(hash(gst_buffer))
+    l_frame=batch_meta.frame_meta_list
+    
+    # Iterate through each frame in the batch metadata until the end
+    while l_frame is not None:
+        try:
+            frame_meta=pyds.NvDsFrameMeta.cast(l_frame.data)
+        except StopIteration:
+            break
+
+        frame_num=frame_meta.frame_num
+        num_obj=frame_meta.num_obj_meta
+        l_obj=frame_meta.obj_meta_list
+        
+        print("Frame Number={} Number of Objects={}".format(frame_num, num_obj))
+        
+        # Append number of objects a list 
+        obj_counts.append(num_obj)
+        
+        # Iterate through each object in the frame metadata until the end
+        while l_obj is not None:
+            try:
+                obj_meta=pyds.NvDsObjectMeta.cast(l_obj.data)
+                print('\t Object: {} - Top: {}, Left: {}, Width: {}, Height: {}'.format(obj_meta.obj_label, \
+                                                                                        round(obj_meta.rect_params.top), \
+                                                                                        round(obj_meta.rect_params.left), \
+                                                                                        round(obj_meta.rect_params.width), \
+                                                                                        round(obj_meta.rect_params.height)))
+            except StopIteration:
+                break
+            
+            try: 
+                l_obj=l_obj.next
+            except StopIteration:
+                break
+        
+        try:
+            l_frame=l_frame.next
+        except StopIteration:
+            break
+    return Gst.PadProbeReturn.OK
+
+# Add probe to inference plugin's source
+pgie_src_pad=pgie.get_static_pad('src')
+probe_id=pgie_src_pad.add_probe(Gst.PadProbeType.BUFFER, pgie_src_pad_buffer_probe)
+print('Attached probe')
